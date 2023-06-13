19d6103bf9c56c0d9fec776dac226784a4cab8f4
Sufian Khan <118069640+ailight360@users.noreply.github.com>
2023-06-13 14:27:00 +0000

    ff

---
+++ script.js
@@ -0,0 +1,37 @@
+function iuFROM() {
+    let text = document.getElementById("iuFROM").value;
+    document.getElementById("ugFROM").innerHTML = text;
+  };
+
+  function uTO() {
+    let text = document.getElementById("uTO").value;
+    document.getElementById("uTOFROM").innerHTML = text;
+  };      
+
+  function uPRODUCT() {
+    let text = document.getElementById("uPRODUCT").value;
+    document.getElementById("uPRODUCTFROM").innerHTML = text;
+  };
+
+
+  function uCOLOR() {
+    let text = document.getElementById("uCOLOR").value;
+    document.getElementById("uCOLORFROM").innerHTML = text;
+  };
+
+  function uSIZE() {
+    let text = document.getElementById("uSIZE").value;
+    document.getElementById("uSIZEFROM").innerHTML = text;
+  };
+
+
+  function uLOT() {
+    let text = document.getElementById("uLOT").value;
+    document.getElementById("uLOTFROM").innerHTML = text;
+  };
+
+
+  function uDATE() {
+    let text = document.getElementById("uDATE").value;
+    document.getElementById("uDATEFROM").innerHTML = text;
+  };
