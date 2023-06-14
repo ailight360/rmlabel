d16d230a6acd9b08afe2c8ad6c304754be2da1f6
Sufian Khan <118069640+ailight360@users.noreply.github.com>
2023-06-14 09:16:07 +0000

    asd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function iuFROM() {
-  let text = document.getElementById("iuFROM").value;
-    document.querySelectorAll(".ugCOMPANY").innerHTML = text;
+  let text = document.getElementsByClassName("iuFROM").value;
+    document.getElementsByClassName("ugCOMPANY").innerHTML = text;
   };
 
   function uTO() {
