e6aa0eee3856515f0532ff5cc96bcc23b2335e00
Sufian Khan <118069640+ailight360@users.noreply.github.com>
2023-06-15 11:48:22 +0000

    update

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ function removeItem(){
 let removeItem = document.getElementById('grids');
 removeItem.removeChild(removeItem.lastElementChild);
 };
+
+function printbtn(){
+var prtContent = document.getElementById("grids");
+var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+WinPrint.document.write(prtContent.innerHTML);
+WinPrint.document.close();
+WinPrint.focus();
+WinPrint.print();
+WinPrint.close();
+};
