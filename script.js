76929bc1eacebe127388c2f04e4f4407aea2f03a
Sufian Khan <118069640+ailight360@users.noreply.github.com>
2023-06-15 11:33:40 +0000

    update

--- script.js
+++ script.js
@@ -1,51 +1,27 @@
-function iuFROM() {
-  let text = document.getElementById("iuFROM").value;
-    document.getElementById("ugCOMPANY").innerHTML = text;
-  };
-
-
-  function uTO() {
-    let text = document.getElementById("uTO").value;
-    document.getElementById("uTOFROM").innerHTML = text;
-  };      
-
-  function uPRODUCT() {
-    let text = document.getElementById("uPRODUCT").value;
-    document.getElementById("uPRODUCTFROM").innerHTML = text;
-  };
-
-
-  function uCOLOR() {
-    let text = document.getElementById("uCOLOR").value;
-    document.getElementById("uCOLORFROM").innerHTML = text;
-  };
-
-  function uSIZE() {
-    let text = document.getElementById("uSIZE").value;
-    document.getElementById("uSIZEFROM").innerHTML = text;
-  };
-
-
-  function uLOT() {
-    let text = document.getElementById("uLOT").value;
-    document.getElementById("uLOTFROM").innerHTML = text;
-  };
-
-
-  function uDATE() {
-    let text = document.getElementById("uDATE").value;
-    document.getElementById("uDATEFROM").innerHTML = text;
-  };
-
-
-  document.getElementById('button').onclick = duplicate;
-
-
-  var i = 0;
-  var original = document.getElementById('duplicater');
-  
-  function duplicate() {
-      var clone = original.cloneNode(true); // "deep" clone
-      clone.id = "duplicetor" + ++i; // there can only be one element with an ID
-      original.parentNode.appendChild(clone);
+function fromInput(){
+  let text = document.getElementById("fromInputName").value;
+  const nodeList = document.querySelectorAll(".fromName");
+  for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].innerHTML = text;
   }
+};
+
+function toInput(){
+  let text = document.getElementById("toInputName").value;
+  const nodeList = document.querySelectorAll(".toName");
+  for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].innerHTML = text;
+  }
+};
+
+let addbutton = document.getElementById("addbutton");
+    addbutton.addEventListener("click", function() {
+    let grids = document.getElementById("grids");
+    let clone = grids.firstElementChild.cloneNode(true);
+    grids.append(clone);
+});
+
+function removeItem(){
+let removeItem = document.getElementById('grids');
+removeItem.removeChild(removeItem.lastElementChild);
+};
